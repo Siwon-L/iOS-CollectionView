ba28e4a663c15d25a6a41d500430c047dbb1b6d3
siwon <91936941+saafaaari@users.noreply.github.com>
2022-05-18 14:22:28 +0900

    Add files via upload

---
+++ Collection/Collection/ViewController.swift
@@ -0,0 +1,157 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by 이시원 on 2022/05/11.
+//
+
+import UIKit
+
+enum Section {
+    case main
+    case seconde
+}
+
+typealias dataSource = UICollectionViewDiffableDataSource<Section, String>
+
+class ViewController: UIViewController {
+    let collectionList = ["swift",
+                          "pencil",
+                          "scribble",
+                          "highlighter",
+                          "lasso",
+                          "folder",
+                          "paperplane",
+                          "note",
+                          "magazine",
+                          "lanyardcard",
+                          "command",
+                          "rosette",
+                          "link",
+                          "person",
+                          "lineweight",
+                          "restart",
+                          "sleep",
+                          "escape",
+                          "option",
+                          "command",
+                          "swift",
+                          "pencil",
+                          "scribble",
+                          "highlighter",
+                          "lasso",
+                          "folder",
+                          "paperplane",
+                          "note",
+                          "magazine",
+                          "lanyardcard",
+                          "command",
+                          "rosette",
+                          "link",
+                          "person",
+                          "lineweight",
+                          "restart",
+                          "sleep",
+                          "escape",
+                          "option",
+                          "command"]
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = configureGridLayout()
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        collectionList.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = .lightGray
+        (cell.contentView.subviews.first?.subviews.last as! UILabel).text = collectionList[indexPath.row]
+        (cell.contentView.subviews.first?.subviews.first as! UIImageView).image = UIImage(systemName: collectionList[indexPath.item])
+        cell.layer.cornerRadius = 10
+        cell.backgroundColor = .white
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor.gray.cgColor
+        
+        return cell
+    }
+        
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    //
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    //        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    //    }
+    //
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    //        let width = collectionView.bounds.width / 2 - 16
+    //        let size = CGSize(width: width, height: width * 1.3)
+    //        return size
+    //    }
+    
+    private func configureGridLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
+            if sectionNumber == 0 {
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1)))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.9), heightDimension: .estimated(200)), subitems: [item])
+                group.contentInsets = .init(top: 0, leading: 5, bottom: 16, trailing: 5)
+                let section = NSCollectionLayoutSection(group: group)
+                section.boundarySupplementaryItems = [.init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .topLeading)]
+                section.orthogonalScrollingBehavior = .groupPaging
+                section.contentInsets = .init(top: 0, leading: 16, bottom: 0, trailing: 16)
+                return section
+            } else if sectionNumber == 1 {
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1)))
+                item.contentInsets = .init(top: 0, leading: 5, bottom: 16, trailing: 5)
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.45), heightDimension: .estimated(200)), subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                section.boundarySupplementaryItems = [.init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .topLeading)]
+                section.orthogonalScrollingBehavior = .groupPaging
+                section.contentInsets = .init(top: 0, leading: 16, bottom: 0, trailing: 16)
+                return section
+            } else {
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+                item.contentInsets = .init(top: 0, leading: 5, bottom: 16, trailing: 5)
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(0.9), heightDimension: .estimated(300)), subitem: item, count: 5)
+                let section = NSCollectionLayoutSection(group: group)
+                section.boundarySupplementaryItems = [.init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .topLeading)]
+                section.orthogonalScrollingBehavior = .groupPaging
+                section.contentInsets = .init(top: 0, leading: 16, bottom: 0, trailing: 16)
+                return section
+            }
+        }
+    }
+    
+    func getItemLayout(widthDimension: NSCollectionLayoutDimension, heightDimension: NSCollectionLayoutDimension) -> NSCollectionLayoutItem {
+        let itemSize = NSCollectionLayoutSize(widthDimension: widthDimension, heightDimension: heightDimension)
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 2, leading: 2, bottom: 2, trailing: 2)
+        return item
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "hhh", for: indexPath)
+            return header
+        } else {
+            return UICollectionReusableView()
+        }
+    }
+}
+
+
+
